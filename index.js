8b0b81a19eafe3c7d0302241e87b2c57f7bc6353
Elite Robust Ontology <erandix9@gmail.com>
2026-02-09 08:37:36 +0300

    Updates

---
+++ index.js
@@ -0,0 +1,44 @@
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
+
+let mainWindow;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    minWidth: 800,
+    minHeight: 600,
+    autoHideMenuBar: true,
+    icon: path.join(__dirname, "public", "icon.png"),
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+    },
+  });
+
+  mainWindow.loadFile(path.join(__dirname, "public", "index.html"));
+
+  mainWindow.webContents.on("before-input-event", (event, input) => {
+    if ((input.control || input.meta) && input.key.toLowerCase() === "p") {
+      event.preventDefault();
+      mainWindow.webContents.print({ printBackground: true });
+    }
+  });
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
